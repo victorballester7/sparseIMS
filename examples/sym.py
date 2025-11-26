3022b599aaa6b0c3cb41fe3ffbcd067703a881a2
Victor Ballester <victor.ballester.ribo@gmail.com>
2025-11-26 13:07:59 +0000

    chol LU on the same gen

--- examples/sym.py
+++ examples/sym.py
@@ -12,7 +12,7 @@ from utils.methods import Cholesky, LU, MatrixInverse
 
 
 @characterise(
-    methods=[Cholesky],
+    methods=[Cholesky,LU],
     measures=[ExecutionTime],
     iterations=10,
     realisations=10,
@@ -40,31 +40,31 @@ def gen_sym(n, *args, **kwargs) -> Tuple[np.ndarray, np.ndarray]:
 
     return A, b
 
-@characterise(
-    methods=[MatrixInverse, LU],
-    measures=[ExecutionTime],
-    iterations=10,
-    realisations=10,
-)
-def gen(n, *args, **kwargs) -> Tuple[np.ndarray, np.ndarray]:
-    eps = 1
-    x0 = 1
-    x1 = 10.0
-    blim = 15.0
+# @characterise(
+#     methods=[MatrixInverse, LU],
+#     measures=[ExecutionTime],
+#     iterations=10,
+#     realisations=10,
+# )
+# def gen(n, *args, **kwargs) -> Tuple[np.ndarray, np.ndarray]:
+#     eps = 1
+#     x0 = 1
+#     x1 = 10.0
+#     blim = 15.0
 
-    # number of negative eigenvalues
-    m = np.random.randint(0, n)
-    lambdas_neg = np.random.uniform(-x1, -x0, m)
-    lambdas_pos = np.random.uniform(x0, x1, n - m)
-    lambdas = np.concatenate((lambdas_neg, lambdas_pos))
+#     # number of negative eigenvalues
+#     m = np.random.randint(0, n)
+#     lambdas_neg = np.random.uniform(-x1, -x0, m)
+#     lambdas_pos = np.random.uniform(x0, x1, n - m)
+#     lambdas = np.concatenate((lambdas_neg, lambdas_pos))
 
-    V = np.random.uniform(0, 1, (n, n)) + eps * np.identity(n)
-    V = np.tril(V)
-    A = la.inv(V) @ np.diag(lambdas) @ V
+#     V = np.random.uniform(0, 1, (n, n)) + eps * np.identity(n)
+#     V = np.tril(V)
+#     A = la.inv(V) @ np.diag(lambdas) @ V
 
-    b = np.random.uniform(-blim, blim, n)
+#     b = np.random.uniform(-blim, blim, n)
 
-    return A, b
+#     return A, b
 
 
 def main():
@@ -77,15 +77,15 @@ def main():
     ]
 
     # ---- Run LU experiments ----
-    results_lu: List[pl.DataFrame] = [gen(d) for d in dimensions]
-    results_lu = [
-        h.with_columns(pl.lit(d).alias("Dimension"))
-        for h, d in zip(results_lu, dimensions)
-    ]
+    # results_lu: List[pl.DataFrame] = [gen(d) for d in dimensions]
+    # results_lu = [
+    #     h.with_columns(pl.lit(d).alias("Dimension"))
+    #     for h, d in zip(results_lu, dimensions)
+    # ]
 
-    # Combine both
-    result = pl.concat(results_sym + results_lu, how="vertical")
-
+    # # Combine both
+    # result = pl.concat(results_sym + results_lu, how="vertical")
+    result = pl.concat(results_sym, how="vertical")
     # ---- Plot ----
     sns.scatterplot(result, x="Dimension", y="ExecutionTime", hue="Method")
     plt.gca().set_yscale("log")
